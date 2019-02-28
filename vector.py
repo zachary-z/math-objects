273297d0d7847d98c34a119271d80e51edffa955
zachary-z <44529926+zachary-z@users.noreply.github.com>
2019-02-27 19:03:05 -0800

    Update vector.py

--- vector.py
+++ vector.py
@@ -4,6 +4,8 @@ import math
 class vec:
 	def __init__(self, *coords):
 		self.coords = coords
+	def __neg__(self):
+		return (-1)*self
 	def __add__(self, other): # Addition
 		return vec(*[self.coords[i]+other.coords[i] for i in range(len(self.coords))])
 	def __radd__(self, other): # Addition
@@ -28,3 +30,9 @@ class vec:
 			   self.coords[0]*other.coords[1]-self.coords[1]*other.coords[0])
 	def __truediv__(self, other): # Scalar division
 		return vec(*[self.coords[i]/other for i in range(len(self.coords))])
+	def __getitem__(self, index): # Support indexing
+		return self.coords[index]
+	def __abs__(self): # Length
+		return math.sqrt(self*self)
+	def normalize(self):
+		return self/abs(self)
